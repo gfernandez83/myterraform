0c077864618acbba7b0f47706bf9bcd73a61a038
glenn fernandez <glenn.fernandez@mnltechnology.com>
2019-01-04 12:08:06 +0800

    update my terraform

--- test_opsworks/opsworks.tf
+++ test_opsworks/opsworks.tf
@@ -1,8 +1,8 @@
 resource "aws_opsworks_stack" "sample" {
   name                              = "my first stack"
   region                            = "ap-northeast-1"
-  service_role_arn                  = "${aws_iam_role.opsworks.arn}"
-  default_instance_profile_arn      = "${aws_iam_instance_profile.opsworks.arn}"
+  service_role_arn                  = "arn:aws:iam::x:role/service-role/aws-opsworks-ec2-role"
+  default_instance_profile_arn      = "arn:aws:iam::x:instance-profile/aws-opsworks-ec2-role"
 
   tags = {
     Name = "first-stack"
